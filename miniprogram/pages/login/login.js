fc698af5ba4f41dbb6b776c030eab140a1c19d05
maidangdang1 <mmt869601840@gmail.com>
2022-04-28 22:47:21 +0800

    feat. login.js修改采用getUserProfile

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -100,22 +100,18 @@ Page({
       },
 
       getUserProfile(e) {
+        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+        // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
         wx.getUserProfile({
-          desc: '用于完善会员资料',
+          desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
           success: (res) => {
             this.setData({
-              userInfo: res.userInfo,
-            })
-          },
-          fail:(res)=>{
-            console.log(e);
-          },
-          complete:(res)=>{
-            console.log(e);
+              userInfo: res.userInfo
+            });
+            console.log(this.data.userInfo);
+            this.check();
           }
-        })
-        console.log(e);
-        this.data.userInfo = e.userInfo;
+        });
       },
 
 
@@ -196,6 +192,7 @@ Page({
                         return false;
                   }
             }
+            console.log(this.data.userInfo);
             wx.showLoading({
                   title: '正在提交',
             })
@@ -207,7 +204,7 @@ Page({
                         email: that.data.email,
                         wxnum: that.data.wxnum,
                         stamp: new Date().getTime(),
-                        info: that.data.userInfo,
+                        info: this.data.userInfo,
                         useful: true,
                         parse: 0,
                   },
